390865a74b6f25ac73839392902c6bff0e68a77e
RamneekSingh24 <ramneeksingh2001@gmail.com>
2022-12-26 14:23:11 +0530

    multi processing queue for async writes

--- test.py
+++ test.py
@@ -1,149 +1,111 @@
 import math
 import time
 
-from redis.client import Redis
+from redis.client import Redis, AsyncWriter
 from redis.key_types import KeyCacheProp
 
+def test_async_writer():
+    r = Redis()
+    r.flushall()
 
-def bench():
-    keys = {"c": KeyCacheProp.WRITE_ONCE}
-    r = Redis(host='localhost', key_types=keys)
+    ar = AsyncWriter()
+
+    for i in range(100):
+        ar.set(str(i), i)
+
+    ar.wait_all()
+
+    for i in range(100):
+        x = r.get(str(i))
+        assert int(x) == i
+
+
+
+def bench_async_writer():
+    r = Redis()
+    r.flushall()
+
+    ar = AsyncWriter()
+
+    def bench(rds):
+        for i in range(10000):
+            x = math.factorial(i % 5000) + 3 // 7
+            rds.set(str(i), x % 100)
+
+    st = time.time()
+    bench(ar)
+    ar.wait_all()
+    ed = time.time()
+
+    print("async writes:", ed - st)
 
     r.flushall()
 
-    r.set('x', 'a')
-    r.set('x:c', 'a')
-    r.get('x:c')
-
     st = time.time()
-    print(r.get('x:c'))
+    bench(r)
     ed = time.time()
-    print((ed - st))
-
-    st = time.time()
-    print(r.get('x'))
-    ed = time.time()
-    print((ed - st))
-
-    print("------------")
-
-    for i in range(10):
-        r.hset('y:c', str(i), str(i))
-        r.hset('y', str(i), str(i))
-
-    for i in range(10):
-        r.hget('y:c', str(i))
-
-    st = time.time()
-    r.hget('y:c', '5')
-    ed = time.time()
-    print((ed - st))
-
-    t1 = ed - st
-
-    st = time.time()
-    r.hget('y', '5')
-    ed = time.time()
-    print((ed - st))
-
-    t2 = ed - st
-
-    print("hget saved:", abs(t2 - t1) * 1000, "ms")
-
-    print("------------")
-
-    st = time.time()
-    r.hgetall('y:c')
-    ed = time.time()
-    print((ed - st))
-
-    t1 = ed - st
-
-    st = time.time()
-    r.hgetall('y')
-    ed = time.time()
-    print((ed - st))
-
-    t2 = ed - st
-
-    print("hgetall saved", abs(t2 - t1) * 1000, "ms")
-
-    print("------------")
-
-    st = time.time()
-    r.hmget('y:c', '1', '2', '3', '4')
-    ed = time.time()
-    print((ed - st) )
-
-    st = time.time()
-    r.hmget('y', '1', '2', '3', '4')
-    ed = time.time()
-    print((ed - st) )
-
-    print("hmget saved", abs(t2 - t1) * 1000, "ms")
-
-
-def test_flush():
-    keys = {"c": KeyCacheProp.WRITE_ONCE}
-    r = Redis(host='localhost', key_types=keys)
-    r.set("kk:c", "kkk")
-    assert r.get("kk:c") is not None
-    print(r.get("kk:c"))
-    r.flushall()
-    assert r.get("kk:c") is None
-
-
-# def test_async_writer():
-#     r = Redis()
-#     r.flushall()
-#
-#     ar = AsyncWriter()
-#
-#     for i in range(100):
-#         ar.set(str(i), i)
-#
-#     ar.wait_all()
-#
-#     for i in range(100):
-#         x = r.get(str(i))
-#         assert int(x) == i
-
-
-
-# def bench_async_writer():
-#     r = Redis()
-#     r.flushall()
-#
-#     ar = AsyncWriter()
-#
-#     def bench(rds):
-#         for i in range(10000):
-#             x = math.factorial(i % 5000) + 3 // 7
-#             rds.set(str(i), x % 100)
-#
-#     st = time.time()
-#     bench(ar)
-#     ar.wait_all()
-#     ed = time.time()
-#
-#     print("async writes:", ed - st)
-#
-#     r.flushall()
-#
-#     st = time.time()
-#     bench(r)
-#     ed = time.time()
-#     print("sync writes:", ed - st)
+    print("sync writes:", ed - st)
 
     #
     #
     # for i in range(1000):
     #     print(r.get(str(i)))
 
-#
-# if __name__ == "__main__":
-#     # bench()
-#     # test_flush()
-#     # print("test")
-#     test_async_writer()
-#     bench_async_writer()
+
+def test_async_pipelined_writer():
+    r = Redis()
+    r.flushall()
+
+    p = r.pipeline()
+
+    p.watch("x")
+    input("press enter to continue")
+    p.multi()
+    p.set("a", 2)
+    p.get("a")
+
+    print(p.execute())
+
+    # ar = AsyncPipelinedWriter()
+    #
+    # for i in range(100):
+    #     ar.set(str(i), i)
+    #
+    # ar.execute()
+    #
+    # ar.wait_all()
+    #
+    # for i in range(100):
+    #     x = r.get(str(i))
+    #     assert int(x) == i
+
+
+if __name__ == "__main__":
+    # bench()
+    # test_flush()
+    # print("test")
+    test_async_writer()
+    bench_async_writer()
+
+
+    # class First(object):
+    #     def __init__(self):
+    #         print("first")
+    #
+    #
+    # class Second():
+    #     def __init__(self):
+    #         print("second")
+    #
+    #
+    # class Third(First):
+    #     def __init__(self):
+    #         print("third")
+    #
+    #
+    # class Fourth(Third, Second):
+    #     def __init__(self):
+    #         super(Third, self).__init__()
+    #         print("that's it")
+    #
+    # f = Fourth()
